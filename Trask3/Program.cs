bde65258e84846decbaf677c398885eb665b28b5
pavelV <psvodnev@gmail.com>
2022-08-04 22:27:05 +0300

    Pr

--- Trask3/Program.cs
+++ Trask3/Program.cs
@@ -5,5 +5,12 @@
 // 4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
 // 2 -> " -2, -1, 0, 1, 2"
 
-Console.Write ("Введите число : ");
-
+Console.Write("Введите число : ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num2 = - num;
+while (num2 <= num)
+{
+    Console.Write(num2+ " ");
+    num2++;
+}
+Console.WriteLine();
