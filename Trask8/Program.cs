cb89809015a144a0151e4d1c0ab6a12f21b1f801
pavelV <psvodnev@gmail.com>
2022-08-05 18:43:57 +0300

    DZ Num 8

--- Trask8/Program.cs
+++ Trask8/Program.cs
@@ -3,3 +3,12 @@
 
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введиет число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int res = 1;
+while (res < N )
+{
+ Console.Write(res + 1);
+ res += 2;
+}
