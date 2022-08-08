a84b543b515ed0427b4e6c64a75a89722cbb0660
pavelV <psvodnev@gmail.com>
2022-08-08 21:49:37 +0300

    name Fail new

---
+++ Task5/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая на вход принимает два числа 
+// и выдаёт, какое число большее, 
+// а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+
+Console.WriteLine("Введите первое чисто: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+
+if (A > B)
+{
+    Console.WriteLine("{0} наибольшее число", A);
+    Console.WriteLine("{0} наименьшее число", B);
+}
+else
+{
+    Console.WriteLine ("{0} наибольшее число", B );
+    Console.WriteLine("{0} наименьшее число", A);
+}
