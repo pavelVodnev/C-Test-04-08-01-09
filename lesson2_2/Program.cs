cbdf1eb6a19b1b8bfe126a6cbd2b04dda337265a
pavelV <psvodnev@gmail.com>
2022-08-09 13:24:28 +0300

    Финал урока

---
+++ lesson2_2/Program.cs
@@ -0,0 +1,50 @@
+﻿void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        // index = index + 1;
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+int[] array = new int[10]; // перевод создай новый маасив в котром будет 10 элиментов.
+
+
+int IndexOff(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine();
+
+int pos = IndexOff(array, 61);// если массива несуществует то будет выходить -1,нужно добавить в 30 строку    int position = -1;
+Console.WriteLine (pos);
