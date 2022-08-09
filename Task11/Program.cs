2384d332565e757bf11fc282490f8d4483ec25bc
pavelV <psvodnev@gmail.com>
2022-08-09 12:13:39 +0300

    добавил сохранения

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -14,6 +14,7 @@ int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число: ");
 int b = Convert.ToInt32(Console.ReadLine());
 
+
 if (a % b == 0 )
 {
     Console.Write($"кратно {a},{b} ");
