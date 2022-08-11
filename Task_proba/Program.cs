29a0168d8ed59a66b8316b5421f2100930a23304
pavelV <psvodnev@gmail.com>
2022-08-11 22:33:26 +0300

    Proba

---
+++ Task_proba/Program.cs
@@ -0,0 +1,15 @@
+﻿// Просто пример работы Whale.она не кооректно работает
+
+// Console.WriteLine("Просто пример Whiel : "); 
+// int d = 5;
+// int count = 0;
+
+// while (true)
+// {
+//     if (d > 3)
+//     {
+//         System.Console.WriteLine("Yes");
+//         break;
+//     }
+//     count++;
+// }
