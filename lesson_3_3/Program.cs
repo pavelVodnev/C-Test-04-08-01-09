4b2369ac2ae5892751ca435eed0af2e5abe21832
pavelV <psvodnev@gmail.com>
2022-08-18 19:59:54 +0300

    Нужно исправить ошибку,создать массив выстраивает по порядку увеличения

--- lesson_3_3/Program.cs
+++ lesson_3_3/Program.cs
@@ -1,38 +1,38 @@
-﻿// создать массив который выстроет по порядку увелечения символов
+﻿// // создать массив который выстроет по порядку увелечения символов
 
 
-int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1 };
+// int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1 };
 
-void PrintArray(int[] array)
-{
-    int count = array.Length;
-    for (int i = 0; i < count; i++)
-    {
-        Console.WriteLine($"{array[i]}");
-    }
-    Console.WriteLine();
-}
-void SelectionSort(int[] array)
-{
+// void PrintArray(int[] array)
+// {
+//     int count = array.Length;
+//     for (int i = 0; i < count; i++)
+//     {
+//         Console.WriteLine($"{array[i]}");
+//     }
+//     Console.WriteLine();
+// }
+// void SelectionSort(int[] array)
+// {
 
-    for (int i = 0; i < array.Length - 1; i++)
-    {
-        int minPosition = i;
-        for (int j = i + 1; j < array.Length; j++)
-        {
-            if (array[j] < array[minPosition]) ;
-        {
-            minPosition=j;
-        }
-        }
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         int minPosition = i;
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if (array[j] < array[minPosition]) ;
+//         {
+//             minPosition=j;
+//         }
+//         }
 
-        int tempoarray = array[i];
-        array[i] = array[minPosition];
-        array[minPosition] = tempoarray;
-    }
-}
+//         int tempoarray = array[i];
+//         array[i] = array[minPosition];
+//         array[minPosition] = tempoarray;
+//     }
+// }
 
-PrintArray(arr);
-SelectionSort(arr);
+// PrintArray(arr);
+// SelectionSort(arr);
 
-PrintArray(arr);
+// PrintArray(arr);
