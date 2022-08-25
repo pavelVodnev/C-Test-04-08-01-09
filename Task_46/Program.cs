81f9888133fd2ed31d97f605518d57f36731d706
pavelV <psvodnev@gmail.com>
2022-08-25 21:59:39 +0300

    Task 46, Задайте двумерный массив размером m×n, заполненный случайными целыми числами.

---
+++ Task_46/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 46: 
+// Задайте двумерный массив размером m×n,
+// заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4  8 19
+// 5 -2 33 -2
+// 77 3  8  1
+
+
+
+int[,] CreateMatrixRndInt(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // строки - row
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // столбцы- col
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            // if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i,j], 4},");
+            // else Console.Write($"{matrix[i,j], 4}");
+
+            Console.Write(j < matrix.GetLength(1) - 1 ? //можно вместо if,else
+            $"{matrix[i,j], 4}," : $"{matrix[i,j], 4}"); // , 4}"); отступы на консоле
+        }
+        Console.WriteLine("]");
+    }
+}
+
+
+int[,] array2D = CreateMatrixRndInt(3, 4, -99, 99);
+PrintMatrix(array2D);
