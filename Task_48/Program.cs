c463bd01c73bd5600c505864a82552bb987f4fbd
pavelV <psvodnev@gmail.com>
2022-08-25 22:00:35 +0300

    Task 48, двумерный массив размера m на n,каждый элемент в массиве находится по формуле: Amn = m+n.

---
+++ Task_48/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 48:
+//  Задайте двумерный массив размера m на n,
+// каждый элемент в массиве находится по формуле: Amn = m+n.
+// Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+
+int[,] CreateMatrix(int row, int col)
+{
+    int[,] matrix = new int[row, col];
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // строки - row
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // столбцы- col
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+    return matrix;
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            // if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i,j], 4},");
+            // else Console.Write($"{matrix[i,j], 4}");
+
+            Console.Write(j < matrix.GetLength(1) - 1 ?  //можно вместо if,else
+            $"{matrix[i, j],4}," : $"{matrix[i, j],4}"); // можно так переносить
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] array2D = CreateMatrix(3, 4);
+PrintMatrix(array2D);
