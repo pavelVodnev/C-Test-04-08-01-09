902f9dff75f464c1e3ff837562b7c036e0c740de
pavelV <psvodnev@gmail.com>
2022-08-30 15:51:50 +0300

    Task 53,программу,которая поменяет местами первую и последнюю строку массива.

---
+++ Task_53/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 53: 
+// Задайте двумерный массив. 
+// Напишите программу,которая поменяет местами первую и последнюю строку массива.
+
+// ...............................................1 вариант.
+
+int[,] CreateMatrixRndInt(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++) // строки - row
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // столбцы- col
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            // if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i,j], 4},");
+            // else Console.Write($"{matrix[i,j], 4}");
+
+            Console.Write(j < matrix.GetLength(1) - 1 ? //можно вместо if,else
+            $"{matrix[i, j],4}," : $"{matrix[i, j],4}");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+
+void ChangeColMatrix (int[,] array)
+{
+    int temp = 0;
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        temp = array[0, i];
+        array[0, i] = array[array.GetLength(0)-1, i];
+        array[array.GetLength(0)-1, i] = temp;
+    }
+}
+
+int[,] array2d = CreateMatrixRndInt(4, 5, 0, 10);
+PrintMatrix(array2d);
+ChangeColMatrix(array2d);
+Console.WriteLine();
+PrintMatrix(array2d);
--- Task_proba/Program.cs
+++ Task_proba/Program.cs
@@ -1,15 +1,37 @@
-﻿// Просто пример работы Whale.она не кооректно работает
+// помощь в решении задачa 47
 
-// Console.WriteLine("Просто пример Whiel : "); 
-// int d = 5;
-// int count = 0;
+double[,] CreateMatrixRndDouble(int row, int col, int min, int max)
+{
+    double[,] matrix = new double[row, col];
+    Random rnd = new Random();
 
-// while (true)
-// {
-//     if (d > 3)
-//     {
-//         System.Console.WriteLine("Yes");
-//         break;
-//     }
-//     count++;
-// }
+    for (int i = 0; i < matrix.GetLength(0); i++) // строки - row
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++) // столбцы- col
+        {
+            double rand = rnd.NextDouble() * (max - min) + min;
+            matrix[i, j] = Math.Round(rand, 2);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            // if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i,j], 4},");
+            // else Console.Write($"{matrix[i,j], 4}");
+
+            Console.Write(j < matrix.GetLength(1) - 1 ? //можно вместо if,else
+            $"{matrix[i, j],6} ;" : $"{matrix[i, j],6}"); // , 4}"); отступы на консоле
+        }
+        Console.WriteLine("]");
+    }
+}
+
+double[,] array2D = CreateMatrixRndDouble(3, 4, 10, 40);
+PrintMatrix(array2D);
